995a77b1fb05f838cc5b82bba6e49a4db1afef73
Sudarshan Sunder <suddu61@gmail.com>
2017-06-29 00:44:02 +0530

    Completed level 3

--- Level3/solution.py
+++ Level3/solution.py
@@ -1,46 +1,22 @@
-class Node(object):
-	def __init__(self, val):
-		self.val = val
-		self.left = None
-		self.right = None
-
-def create_tree(root, h, cur_h):
-	if cur_h >= h:
-		return
-	root.left = Node(0)
-	root.right = Node(0)
-	create_tree(root.left, h, cur_h+1)
-	create_tree(root.right, h, cur_h+1)
-
-def postorder(root):
-	if root is None:
-		return
-	postorder(root.left)
-	postorder(root.right)
-	root.val = postorder.count
-	postorder.count += 1
-
-def parent(root, val):
-	lh = -1
-	rh = -1
-	if root is None:
-		return -1
-	if root.left is not None and root.left.val == val:
-		return root.val
-	if root.right is not None and root.right.val == val:
-		return root.val
-	lh = parent(root.left, val)
-	rh = parent(root.right, val)
-	return lh if lh != -1 else rh
+def loc(h, tar, cur_h):
+	cur_h /= 2
+	right = h - 1
+	left = h - 1 - cur_h
+	cur_h -= 1
+	if right == tar or left == tar:
+		return h
+	else:
+		if tar <= left:
+			return loc(left,tar,cur_h);
+		else:
+			return loc(right,tar,cur_h);
 
 def answer(h, q):
-	root = Node(0)
-	create_tree(root, h-1, 0)
-	postorder.count = 1
-	postorder(root)
+	num = 2**h - 1
 	p = []
 	for val in q:
-		p.append(parent(root, val))
+		if val < num and val >=1:
+			p.append(loc(num,val,num-1))
+		else:
+			p.append(-1);
 	return p
-
-print answer(30, [1,2,3])
